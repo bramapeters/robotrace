4c14e1b96f46feabc4975e4a2690889e84cf739d
Bram Peters <bramapeters@hotmail.com>
2019-10-25 20:05:29 +0200

    track texture added including shading
    track texture added including shading

--- src/robotrace/RaceTrack.java
+++ src/robotrace/RaceTrack.java
@@ -1,6 +1,10 @@
 package robotrace;
 
 import static com.jogamp.opengl.GL.GL_LINE_LOOP;
+import static com.jogamp.opengl.GL.GL_REPEAT;
+import static com.jogamp.opengl.GL.GL_TEXTURE_2D;
+import static com.jogamp.opengl.GL.GL_TEXTURE_WRAP_S;
+import static com.jogamp.opengl.GL.GL_TEXTURE_WRAP_T;
 import static com.jogamp.opengl.GL.GL_TRIANGLE_STRIP;
 import com.jogamp.opengl.util.gl2.GLUT;
 import com.jogamp.opengl.GL2;
@@ -31,13 +35,10 @@ abstract class RaceTrack {
     public void draw(GL2 gl, GLU glu, GLUT glut, GlobalState gs, double trackNr) {
         // If gs.trackNr = 0, the parametrics track is drawn.
         if(trackNr==0){
-            
             System.out.println("trackNr="+gs.trackNr);
-
             int N=40, Ntracks=4, Ncorners=2;    
             int tmin=0;        
             double dt = Math.pow(N, -1);
-
             drawTrack(gl, glu, glut, tmin, dt, N, Ntracks, Ncorners);
             drawBrick(gl, glu,glut, tmin, dt, N, Ntracks, Ncorners);
             /** Draw Ntracks+1 lines that separate each track. */
@@ -51,7 +52,7 @@ abstract class RaceTrack {
                     Vector Normal = new Vector(0,0,1);
                     Vector Tangent = getTangent(tmin+i*dt);
                     Vector Bitangent = Normal.cross(Tangent);
-                    gl.glVertex3d(P.x+(k-3)*laneWidth*Bitangent.x,P.y+(k-3)*laneWidth*Bitangent.y,P.z+(k-3)*laneWidth*Bitangent.z);     
+                    gl.glVertex3d(P.x+(k-2)*laneWidth*Bitangent.x,P.y+(k-2)*laneWidth*Bitangent.y,P.z+(k-2)*laneWidth*Bitangent.z);     
                 }
                 gl.glEnd();
             }
@@ -78,7 +79,7 @@ abstract class RaceTrack {
                     Vector Normal = new Vector(0,0,1);
                     Vector Tangent = getTangent(tmin+i*dt);
                     Vector Bitangent = Normal.cross(Tangent);
-                    gl.glVertex3d(P.x+(k-3)*laneWidth*Bitangent.x,P.y+(k-3)*laneWidth*Bitangent.y,P.z+(k-3)*laneWidth*Bitangent.z);     
+                    gl.glVertex3d(P.x+(k-2)*laneWidth*Bitangent.x,P.y+(k-2)*laneWidth*Bitangent.y,P.z+(k-2)*laneWidth*Bitangent.z);     
                 }
                 gl.glEnd();
                 gl.glFlush();
@@ -87,61 +88,135 @@ abstract class RaceTrack {
     }
     
     public void drawTrack(GL2 gl, GLU glu, GLUT glut, int tmin, double dt, int N, int Ntracks, int Ncorners){
+        ShaderPrograms.trackShader.useProgram(gl);
+        Textures.track.bind(gl);
+        gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        for (int i = 0; i < 100; i++){
+            gl.glBegin(GL_TRIANGLE_STRIP);
+            for (int j = -2; j <= 2; j++) {
+                double i_d = i * 0.01;
+                gl.glNormal3d(Vector.Z.x, Vector.Z.y, Vector.Z.z);
+                Vector N1 = getTangent(i_d).cross(Vector.Z).normalized();
+                Vector P1 = getPoint(i_d).add(N1.scale(j * laneWidth));
+                Vector N2 = getTangent(i_d + 0.01).cross(Vector.Z).normalized();
+                Vector P2 = getPoint(i_d + 0.01).add(N2.scale(j * laneWidth));
+                
+                double texx = 0.25 * (j + 2) ;
+                double texy = 10 * i_d;     
+                gl.glTexCoord2d(texx, texy);
+                gl.glVertex3d(P1.x, P1.y, P1.z);
+                texy = 10 * (i_d + 0.01);
+                gl.glTexCoord2d(texx, texy);
+                gl.glVertex3d(P2.x, P2.y, P2.z);
+            }
+            gl.glEnd();
+            gl.glFlush();
+        }
+        
+        
         /** Draw surface of the track at z=1*/
+        /*
         gl.glBegin(GL_TRIANGLE_STRIP);
-        gl.glColor3f(255, 0, 0);   
+        gl.glColor3f(255, 0, 0);
         for (int i=0; i<=N; i++){
             Vector P = getPoint(tmin+i*dt);
             Vector Normal = new Vector(0,0,1);
             Vector Tangent = getTangent(tmin+i*dt);
             Vector Bitangent = Normal.cross(Tangent);
-            gl.glVertex3d(P.x+1*laneWidth*Bitangent.x,P.y+1*laneWidth*Bitangent.y,1);                
-            gl.glVertex3d(P.x-3*laneWidth*Bitangent.x,P.y-3*laneWidth*Bitangent.y,1);                
+            gl.glVertex3d(P.x+2*laneWidth*Bitangent.x,P.y+2*laneWidth*Bitangent.y,1);                
+            gl.glVertex3d(P.x-2*laneWidth*Bitangent.x,P.y-2*laneWidth*Bitangent.y,1);                
         }
         gl.glEnd();
         gl.glFlush();
         
         /** Draw the surface of track at z=-1. */
+        /*
         gl.glBegin(GL_TRIANGLE_STRIP);
         for (int i=0; i<=N; i++){
             Vector P = getPoint(tmin+i*dt);
             Vector Normal = new Vector(0,0,1);
             Vector Tangent = getTangent(tmin+i*dt);
             Vector Bitangent = Normal.cross(Tangent);
-            gl.glVertex3d(P.x+1*laneWidth*Bitangent.x,P.y+1*laneWidth*Bitangent.y,-1);                
-            gl.glVertex3d(P.x-3*laneWidth*Bitangent.x,P.y-3*laneWidth*Bitangent.y,-1);  
+            gl.glVertex3d(P.x+2*laneWidth*Bitangent.x,P.y+2*laneWidth*Bitangent.y,-1);                
+            gl.glVertex3d(P.x-2*laneWidth*Bitangent.x,P.y-2*laneWidth*Bitangent.y,-1);  
         }
         gl.glEnd();
-        gl.glFlush();
+        gl.glFlush();*/
     }
     
     public void drawBrick(GL2 gl, GLU glu, GLUT glut, int tmin, double dt, int N, int Ntracks, int Ncorners){
-                /** Draw the surface of the inner side of track. */
+        ShaderPrograms.trackShader.useProgram(gl);
+        Textures.brick.bind(gl);
+        gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        for (int i = 0; i < 100; i++){
+            double i_d = i * 0.01;
+            gl.glBegin(GL_TRIANGLE_STRIP);
+                Vector N1 = getTangent(i_d).cross(Vector.Z).normalized();
+                Vector P1 = getPoint(i_d).add(N1.scale(2 * laneWidth));
+                Vector N2 = getTangent(i_d + 0.01).cross(Vector.Z).normalized();
+                Vector P2 = getPoint(i_d + 0.01).add(N2.scale(2 * laneWidth));
+                gl.glNormal3d(Vector.Z.x, Vector.Z.y, Vector.Z.z);
+                gl.glTexCoord2d(0, 0);
+                gl.glNormal3d(N1.x, N1.y, N1.z);
+                gl.glVertex3d(P1.x, P1.y, P1.z);
+                gl.glTexCoord2d(0, 1);
+                gl.glVertex3d(P1.x, P1.y, P1.z - 1);
+                gl.glTexCoord2d(1, 0);
+                gl.glNormal3d(N2.x, N2.y, N2.z);
+                gl.glVertex3d(P2.x, P2.y, P2.z);
+                gl.glTexCoord2d(1, 1);
+                gl.glVertex3d(P2.x, P2.y, P2.z - 1);
+            gl.glEnd();
+            gl.glBegin(GL_TRIANGLE_STRIP);
+                Vector N3 = getTangent(i_d).cross(Vector.Z).normalized();
+                Vector P3 = getPoint(i_d).add(N3.scale(-2*laneWidth));
+                Vector N4 = getTangent(i_d + 0.01).cross(Vector.Z).normalized();
+                Vector P4 = getPoint(i_d + 0.01).add(N4.scale(-2*laneWidth));
+                gl.glTexCoord2d(0, 0);
+                gl.glNormal3d(-N3.x, -N3.y, -N3.z);
+                gl.glVertex3d(P3.x, P3.y, P3.z);
+                gl.glTexCoord2d(0, 1);
+                gl.glVertex3d(P3.x, P3.y, P3.z - 1);
+                gl.glTexCoord2d(1, 0);
+                gl.glNormal3d(-N4.x, -N4.y, -N4.z);
+                gl.glVertex3d(P4.x, P4.y, P4.z);
+                gl.glTexCoord2d(1, 1);
+                gl.glVertex3d(P4.x, P4.y, P4.z - 1);
+            gl.glEnd();
+        }
+        /*
+        /** Draw the surface of the inner side of track. */
+        /*
         gl.glBegin(GL_TRIANGLE_STRIP);
         for (int i=0; i<=N; i++){
             Vector P = getPoint(tmin+i*dt);
             Vector Normal = new Vector(0,0,1);
             Vector Tangent = getTangent(tmin+i*dt);
             Vector Bitangent = Normal.cross(Tangent);
-            gl.glVertex3d(P.x+1*laneWidth*Bitangent.x,P.y+1*laneWidth*Bitangent.y,1);                
-            gl.glVertex3d(P.x+1*laneWidth*Bitangent.x,P.y+1*laneWidth*Bitangent.y,-1); 
+            gl.glVertex3d(P.x+2*laneWidth*Bitangent.x,P.y+2*laneWidth*Bitangent.y,1);                
+            gl.glVertex3d(P.x+2*laneWidth*Bitangent.x,P.y+2*laneWidth*Bitangent.y,-1); 
         }
         gl.glEnd();
         gl.glFlush();
+        */
         
         /** Draw the surface of the outer side of the track. */
+        /*
         gl.glBegin(GL_TRIANGLE_STRIP);
         for (int i=0; i<=N; i++){
             Vector P = getPoint(tmin+i*dt);
             Vector Normal = new Vector(0,0,1);
             Vector Tangent = getTangent(tmin+i*dt);
             Vector Bitangent = Normal.cross(Tangent);
-            gl.glVertex3d(P.x-3*laneWidth*Bitangent.x,P.y-3*laneWidth*Bitangent.y,1);                
-            gl.glVertex3d(P.x-3*laneWidth*Bitangent.x,P.y-3*laneWidth*Bitangent.y,-1);  
+            gl.glVertex3d(P.x-2*laneWidth*Bitangent.x,P.y-2*laneWidth*Bitangent.y,1);                
+            gl.glVertex3d(P.x-2*laneWidth*Bitangent.x,P.y-2*laneWidth*Bitangent.y,-1);  
         }
         gl.glEnd();
-        gl.glFlush();
+        gl.glFlush();*/
     }
+
     
     /**
      * Returns the center of a lane at 0 <= t < 1.
@@ -152,9 +227,8 @@ abstract class RaceTrack {
         Vector Normal = new Vector(0,0,1);
         Vector Tangent = getTangent(t);
         Vector Bitangent = Normal.cross(Tangent);
-        
 
-        Vector P = new Vector(Pcenter.x+(lane-2.5)*laneWidth*Bitangent.x,Pcenter.y+(lane-2.5)*laneWidth*Bitangent.y,1);                
+        Vector P = new Vector(Pcenter.x+(lane-1.5)*laneWidth*Bitangent.x,Pcenter.y+(lane-1.5)*laneWidth*Bitangent.y,1);                
         //System.out.println("P="+P);
         return P;
 
